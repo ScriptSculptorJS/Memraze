40356c0a28fcae1352f1c4b214de21497553686a
ScriptSculptorJS <cmcunningham27@yahoo.com>
2025-06-05 11:06:06 -0700

    Install and import/link bootstrap and react bootstrap

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,4 +1,5 @@
 import { Routes, Route } from 'react-router-dom';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import Login from './pages/Login.jsx';
 import Profile from './components/profile.jsx';
 
