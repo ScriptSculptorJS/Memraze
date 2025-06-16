d5dfe5f8fb8c4c73485a4b8b985709141b8aa32f
ScriptSculptorJS <cmcunningham27@yahoo.com>
2025-06-16 12:58:54 -0700

    Updating component state with new Tab's title and description

--- frontend/src/components/tab-info/Tab-info.jsx
+++ frontend/src/components/tab-info/Tab-info.jsx
@@ -1,3 +1,4 @@
+import { useState } from 'react';
 import Col from 'react-bootstrap/Col';
 import Tab from 'react-bootstrap/Tab';
 import Form from 'react-bootstrap/Form';
@@ -5,6 +6,15 @@ import Button from 'react-bootstrap/Button';
 import './tab-info.css';
 
 function TabInfo() {
+  const [ newTab, setNewTab ] = useState({
+    title: '',
+    description: '',
+  })
+  
+  function handleNewTab(e) {
+    e.preventDefault();
+    console.log(newTab)
+  }
 
   return(
     <Col sm={9} lg={10} className='tab-info-container'>
@@ -14,21 +24,45 @@ function TabInfo() {
             Let's get you started with your new Tab!
           </h6>
           <Form className='newTabForm'>
-            <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
-              <Form.Label>Enter your Tab title</Form.Label>
-              <Form.Control size='sm' as='textarea' rows={1} />
+            <Form.Group 
+              className="mb-3" controlId="exampleForm.ControlInput1"
+            >
+              <Form.Label>
+                Enter your Tab title
+              </Form.Label>
+              <Form.Control 
+                className='tab-text'  as='textarea' 
+                rows={1} 
+                onChange={e => setNewTab({ ...newTab, title: e.target.value })} />
             </Form.Group>
-            <Form.Group className="mb-3" controlId="exampleForm.ControlTextarea1">
+            <Form.Group 
+              className="mb-3" controlId="exampleForm.ControlTextarea1"
+            >
               <Form.Label>Enter a brief description to highlight why you are creating this Tab</Form.Label>
-              <Form.Control as="textarea" rows={3} />
+              <Form.Control 
+                className='tab-text' as="textarea" 
+                rows={3} 
+                onChange={e => setNewTab({ ...newTab, description: e.target.value })} />
             </Form.Group>
-            <Button className='button' size='sm'  type="submit">Create</Button>
+            <Button 
+              size='sm' 
+              className='button' 
+              onClick={handleNewTab}
+            >
+              Create
+            </Button>
           </Form>
         </Tab.Pane>
-        <Tab.Pane eventKey="second" className='tab-text'>
+        <Tab.Pane 
+          eventKey="second" 
+          className='tab-text'
+        >
           First tab content
         </Tab.Pane>
-        <Tab.Pane eventKey="third" className='tab-text'>
+        <Tab.Pane 
+          eventKey="third" 
+          className='tab-text'
+        >
           Second tab content
         </Tab.Pane>
       </Tab.Content>
