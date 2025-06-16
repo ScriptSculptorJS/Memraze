a7cf77aabca18c0bbbe04a0166f8935d802c02c8
ScriptSculptorJS <cmcunningham27@yahoo.com>
2025-06-16 12:21:33 -0700

    Created tab for creating a new tab which will render a form for the user to enter a title and brief descriptoin about their new tab.

--- frontend/src/components/tab-info/Tab-info.jsx
+++ frontend/src/components/tab-info/Tab-info.jsx
@@ -1,5 +1,7 @@
 import Col from 'react-bootstrap/Col';
 import Tab from 'react-bootstrap/Tab';
+import Form from 'react-bootstrap/Form';
+import Button from 'react-bootstrap/Button';
 import './tab-info.css';
 
 function TabInfo() {
@@ -8,9 +10,25 @@ function TabInfo() {
     <Col sm={9} lg={10} className='tab-info-container'>
       <Tab.Content>
         <Tab.Pane eventKey="first" className='tab-text'>
-          First tab content
+          <h6>
+            Let's get you started with your new Tab!
+          </h6>
+          <Form className='newTabForm'>
+            <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
+              <Form.Label>Enter your Tab title</Form.Label>
+              <Form.Control size='sm' as='textarea' rows={1} />
+            </Form.Group>
+            <Form.Group className="mb-3" controlId="exampleForm.ControlTextarea1">
+              <Form.Label>Enter a brief description to highlight why you are creating this Tab</Form.Label>
+              <Form.Control as="textarea" rows={3} />
+            </Form.Group>
+            <Button className='button' size='sm'  type="submit">Create</Button>
+          </Form>
         </Tab.Pane>
         <Tab.Pane eventKey="second" className='tab-text'>
+          First tab content
+        </Tab.Pane>
+        <Tab.Pane eventKey="third" className='tab-text'>
           Second tab content
         </Tab.Pane>
       </Tab.Content>
--- frontend/src/components/tabs/Tabs.jsx
+++ frontend/src/components/tabs/Tabs.jsx
@@ -6,14 +6,17 @@ function Tabs() {
   return(
     <Col sm={3} lg={2} className='tab-container vh-100'>
       <p className='mt-3 collection'>
-        Your collection
+        Your Collection
       </p>
       <Nav variant="pills" className="flex-column">
         <Nav.Item>
-          <Nav.Link eventKey="first" className='tab-title'>Tab 1</Nav.Link>
+          <Nav.Link eventKey="first" className='tab-title'>Start new Tab</Nav.Link>
         </Nav.Item>
         <Nav.Item>
-          <Nav.Link eventKey="second" className='tab-title'>Tab 2</Nav.Link>
+          <Nav.Link eventKey="second" className='tab-title'>Tab 1</Nav.Link>
+        </Nav.Item>
+        <Nav.Item>
+          <Nav.Link eventKey="third" className='tab-title'>Tab 2</Nav.Link>
         </Nav.Item>
       </Nav>
     </Col>
